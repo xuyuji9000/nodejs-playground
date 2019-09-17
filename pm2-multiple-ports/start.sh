a8c611f7a65ace8ce9378bf0274e87f70149a9ef
Karl Xu <karl.yogi.xu@gmail.com>
2019-09-17 19:09:12 +0800

    issue #4 optimize running multip process

--- pm2-multiple-ports/start.sh
+++ pm2-multiple-ports/start.sh
@@ -4,15 +4,15 @@ Seq 8000 8003 | while read line;
 do
   NAME=index-$line
   pm2 describe $NAME > /dev/null 2>&1
-  if [ -z $? ] 
+  RESULT=$?
+  echo $RESULT
+  if [ $RESULT -eq "0" ]
   then
     pm2 restart $NAME    
   else
     echo $line
     pm2 delete $NAME
     export PORT=$line; pm2 start --name $NAME ./index.js 
-    sleep 5
   fi
-    
-
+  sleep 5
 done
