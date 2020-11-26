8fdc3c9214b712d15b920b1e45b5f338b723afdf
Karl Xu <karl.yogi.xu@gmail.com>
2020-11-26 15:36:24 +0800

    pass commmand line parameters: optimize example

--- pass-command-line-parameters/index.js
+++ pass-command-line-parameters/index.js
@@ -1,12 +1,24 @@
-// Command example:  node index.js --name=karl
+/* 
+Successful command example:  node index.js --name=karl
+Failed command example: node index.js
+*/
 
 var argv = require('minimist')(process.argv.slice(2));
 
 console.log(argv);
 console.log(argv['name']);
 
+if(undefined == argv['name']) {
+    console.error("Parameter name is not defined.")
+    process.exit(1); // [2]
+}
+
+console.log("Parameter passing succeed.")
+
 /**
 # References
 
 1. [Node.js, accept arguments from the command line](https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line)
- */
+
+2. [How to exit from a Node.js program](https://nodejs.dev/learn/how-to-exit-from-a-nodejs-program)
+*/
